2cb1687a9b803ee48f305ecb5b302e996b0cc0b7
Irving Barron <ibarron@ur.rochester.edu>
2020-09-23 02:53:57 -0400

    IB modified boostrat example to create simple polar encoder/decoder

--- examples/bootstrap/src/main.cpp
+++ examples/bootstrap/src/main.cpp
@@ -38,6 +38,7 @@ struct buffers
 	std::vector<float> noisy_symbols;
 	std::vector<float> LLRs;
 	std::vector<int  > dec_bits;
+	std::vector<bool > frozen_bits;//IB added
 };
 void init_buffers(const params &p, buffers &b);
 
@@ -51,65 +52,44 @@ void init_utils(const modules &m, utils &u);
 
 int main(int argc, char** argv)
 {
-	// get the AFF3CT version
-	const std::string v = "v" + std::to_string(tools::version_major()) + "." +
-	                            std::to_string(tools::version_minor()) + "." +
-	                            std::to_string(tools::version_release());
-
-	std::cout << "#----------------------------------------------------------"      << std::endl;
-	std::cout << "# This is a basic program using the AFF3CT library (" << v << ")" << std::endl;
-	std::cout << "# Feel free to improve it as you want to fit your needs."         << std::endl;
-	std::cout << "#----------------------------------------------------------"      << std::endl;
-	std::cout << "#"                                                                << std::endl;
-
+	
 	params p;  init_params (p   ); // create and initialize the parameters defined by the user
 	modules m; init_modules(p, m); // create and initialize the modules
 	buffers b; init_buffers(p, b); // create and initialize the buffers required by the modules
 	utils u;   init_utils  (m, u); // create and initialize the utils
 
-	// display the legend in the terminal
-	u.terminal->legend();
+	// Create Frozen Bits Generator GA
+	tools::Frozenbits_generator_GA         frozenBitsGeneratorGA(p.K,p.N);
 
-	// loop over the various SNRs
-	for (auto ebn0 = p.ebn0_min; ebn0 < p.ebn0_max; ebn0 += p.ebn0_step)
-	{
-		// compute the current sigma for the channel noise
-		const auto esn0  = tools::ebn0_to_esn0 (ebn0, p.R);
-		const auto sigma = tools::esn0_to_sigma(esn0     );
+	// Compute the noise (sigma)
+	float SNR_max  =  10.01f;
+    const auto esn0  = tools::ebn0_to_esn0 (SNR_max, p.R);
+    const auto sigma = tools::esn0_to_sigma(esn0;
+    u.noise->set_noise(sigma, SNR_max, esn0);
 
-		u.noise->set_noise(sigma, ebn0, esn0);
+	// Set the noise for the Frozen Bits Generator GA
+	frozenBitsGeneratorGA.set_noise(*u.noise);
+	
+	// Generate the frozen bits
+	frozenBitsGeneratorGA.generate(myB.frozen_bits);
 
-		// update the sigma of the modem and the channel
-		m.modem  ->set_noise(*u.noise);
-		m.channel->set_noise(*u.noise);
+	// Create Polar Encoder
+	module::Encoder_polar<> polarEncoder(p.K, p.N, b.frozen_bits);
 
-		// display the performance (BER and FER) in real time (in a separate thread)
-		u.terminal->start_temp_report();
+	// Generate Random bits
+	b.source ->generate(b.ref_bits;
+	
+	// Encode the bits
+	polarEncoder.encode(b.ref_bits,b.enc_bits);
 
-		// run the simulation chain
-		while (!m.monitor->fe_limit_achieved() && !u.terminal->is_interrupt())
-		{
-			m.source ->generate    (                 b.ref_bits     );
-			m.encoder->encode      (b.ref_bits,      b.enc_bits     );
-			m.modem  ->modulate    (b.enc_bits,      b.symbols      );
-			m.channel->add_noise   (b.symbols,       b.noisy_symbols);
-			m.modem  ->demodulate  (b.noisy_symbols, b.LLRs         );
-			m.decoder->decode_siho (b.LLRs,          b.dec_bits     );
-			m.monitor->check_errors(b.dec_bits,      b.ref_bits     );
-		}
+	// Create Polar Decoder
+	module::Decoder_polar_SC_naive<> polarDecoder(p.K, p.N, myB.frozen_bits);
+	
+	// Convert the vector<int> to vector<float> such that it can be used in the decoder
+	std::vector<float> encodedBits(myB.enc_bits.begin(), myB.enc_bits.end());
 
-		// display the performance (BER and FER) in the terminal
-		u.terminal->final_report();
-
-		// reset the monitor for the next SNR
-		m.monitor->reset();
-		u.terminal->reset();
-
-		// if user pressed Ctrl+c twice, exit the SNRs loop
-		if (u.terminal->is_over()) break;
-	}
-
-	std::cout << "# End of the simulation" << std::endl;
+	// Decode the encoded bits
+	polarDecoder.decode_siho(encodedBits,myB.dec_bits);
 
 	return 0;
 }
@@ -139,6 +119,7 @@ void init_modules(const params &p, modules &m)
 	m.monitor = std::unique_ptr<module::Monitor_BFER          <>>(new module::Monitor_BFER          <>(p.K, p.fe  ));
 };
 
+
 void init_buffers(const params &p, buffers &b)
 {
 	b.ref_bits      = std::vector<int  >(p.K);
@@ -147,6 +128,7 @@ void init_buffers(const params &p, buffers &b)
 	b.noisy_symbols = std::vector<float>(p.N);
 	b.LLRs          = std::vector<float>(p.N);
 	b.dec_bits      = std::vector<int  >(p.K);
+	b.frozen_bits   = std::vector<bool >(p.N);//IB added
 }
 
 void init_utils(const modules &m, utils &u)
